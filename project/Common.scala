4ad7b03e642846343d855c8e1fd943d24630c7f7
Untoldwind <landru@untoldwind.net>
2014-04-14 18:46:04 +0200

    Publish to sonatype

--- project/Common.scala
+++ project/Common.scala
@@ -9,12 +9,19 @@ object Common {
 
                       scalaVersion := "2.10.3",
 
-                      publishTo :=
-                        Some("Bintray" at "https://api.bintray.com/maven/untoldwind/maven/toehold")
+                      publishTo := {
+                        val nexus = "https://oss.sonatype.org/"
+                        if (version.value.trim.endsWith("SNAPSHOT"))
+                          Some("Sonatype Nexus Repository Manager" at nexus + "content/repositories/snapshots")
+                        else
+                          Some("Sonatype Nexus Repository Manager"  at nexus + "service/local/staging/deploy/maven2")
+                      }
+//                        publishTo :=
+//                        Some("Bintray" at "https://api.bintray.com/maven/untoldwind/maven/toehold")
                     )
 
   val publishSettings = Seq(
-                             credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+                             credentials += Credentials(Path.userHome / ".ivy2" / ".credentials-sonatype")
 
                            ) ++ aether.Aether.aetherPublishSettings
 }
